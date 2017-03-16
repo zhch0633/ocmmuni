6702a090f25faafe874357da9f14e2c7e65d426d
zhch0633 <zhch0633@live.cn>
2017-03-16 13:46:31 +0800

    commit cmake prototype

--- Core/TCP/src/MathFunctions.cpp
+++ Core/TCP/src/MathFunctions.cpp
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 /**
  * power - Calculate the power of number.
  * @param base: Base value.
@@ -11,7 +9,6 @@ double power(double base, int exponent)
 {
     int result = base;
     int i;
-
     if (exponent == 0) {
         return 1;
     }
@@ -20,16 +17,4 @@ double power(double base, int exponent)
         result = result * base;
     }
     return result;
-}
-int main(int argc, char *argv[])
-{
-    if (argc < 3){
-        printf("Usage: %s base exponent \n", argv[0]);
-        return 1;
-    }
-    double base = atof(argv[1]);
-    int exponent = atoi(argv[2]);
-    double result = power(base, exponent);
-    printf("%g ^ %d is %g\n", base, exponent, result);
-    return 0;
 }
