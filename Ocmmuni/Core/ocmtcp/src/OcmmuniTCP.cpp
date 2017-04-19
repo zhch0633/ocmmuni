ed106481e9a00f52aa0df3014a99dc3a044ed167
chen.zhang <chen.zhang@alibaba-inc.com>
2017-04-19 10:44:29 +0800

    commit add demo server

--- Ocmmuni/Core/ocmtcp/src/OcmmuniTCP.cpp
+++ Ocmmuni/Core/ocmtcp/src/OcmmuniTCP.cpp
@@ -2,17 +2,38 @@
 // Created by zhch0633 on 16/03/17.
 //
 #include "OcmmuniTCP.h"
-#include <sys/socket.h>
 #include <iostream>
 #include <asio.hpp>
+#include "server.hpp"
+
+#include "asio.hpp"
+#include <string>
 
 using namespace asio;
 
 void HelloFunc(){
-    asio::io_service io;
+    std::cout << "start server";
 
+    try
+    {
+        // Check command line arguments.
+        std::cerr << "Usage: http_server <address> <port> <doc_root>\n";
+        std::cerr << "  For IPv4, try:\n";
+        std::cerr << "    receiver 0.0.0.0 80 .\n";
+        std::cerr << "  For IPv6, try:\n";
+        std::cerr << "    receiver 0::0 80 .\n";
 
-    std::cout << "Hello ASIO" << std::endl;
+        // Initialise the server.
+        http::server::server s("0.0.0.0", "80", "..");
+
+        // Run the server until stopped.
+        s.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "exception: " << e.what() << "\n";
+    }
+
 }
 
 void TestSocket()
